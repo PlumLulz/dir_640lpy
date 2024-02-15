bf3e3a9512cbf2645cc5217d5a8d3bad834b790d
Plum <plumlulz@gmail.com>
2024-02-14 22:51:45 -0700

    Update dir_640l.py

--- dir_640l.py
+++ dir_640l.py
@@ -6,18 +6,82 @@
 import hashlib
 import argparse
 
+def passgen(input, pwd_length, mode):
+
+	numbers = "0123456789"
+	vowels_lc = "aeiou"
+	vowels_uc = "AEIOU"
+	cons_lc = "bcdfghjklmnpqrstvwxyz"
+	cons_uc = "BCDFGHJKLMNPQRSTVWXYZ"
+	hexx = "abcdef"
+
+	if mode == 1:
+		charset = vowels_lc + cons_lc
+	elif mode == 2:
+		charset = vowels_lc + vowels_uc + cons_lc + cons_uc
+	elif mode == 3:
+		charset = cons_uc
+	elif mode == 4:
+		charset = vowels_lc + vowels_uc + cons_lc + cons_uc + numbers
+	elif mode == 5:
+		charset = numbers
+	elif mode == 6:
+		charset = numbers + hexx
+
+	hashh = hashlib.md5()
+	hashh.update(input.encode())
+	digest = hashh.digest()
+
+	pwd = ''
+	for i in range(pwd_length):
+		hashh2 = hashlib.md5()
+		hashh2.update(digest)
+		new_digest = hashh2.digest()
+		long_int = 0
+		long_int = long_int + new_digest[0]
+		long_int = long_int + new_digest[1] * 2 ** 8
+		long_int = long_int + new_digest[2] * 2 ** 16
+		long_int = long_int + new_digest[3] * 2 ** 24
+
+		char_pos = long_int % len(charset)
+		letter = charset[char_pos]
+		pwd += letter
+		if mode == 3:
+			if len(charset) == 5:
+				charset = cons_lc
+			else:
+				charset = vowels_lc
+		digest = new_digest
+	return pwd
+
 def dir_640l(mac):
 	
-	input_string = []
+	last_mac_byte = int(mac[-2:], 16)
+	pwd_mode = last_mac_byte % 5
+
+	mac_order = [1, 2, 3, 4, 5, 6]
+	if pwd_mode == 2 or pwd_mode == 4:
+		mac_order = [3, 2, 1, 6, 5, 4]
+	elif pwd_mode == 0:
+		mac_order = [6, 5, 4, 3, 2, 1]
+
+
+	input_string2 = []
 	for i in range(0, 12, 2):
-		input_string.append(mac[i:i+2].upper())
+		input_string2.append(mac[i:i+2].upper())
+	input_string = []
+	for i in mac_order:
+		input_string.append(input_string2[i-1])
 
 	input_string = "_"+"_".join(input_string).upper()
 	input_string += chr(10)
 
-	hashh = hashlib.sha1()
-	hashh.update(input_string.encode())
-	hashh = list(hashh.hexdigest())
+	if pwd_mode == 0:
+		hashh = passgen(input_string, 32, 6)
+	else:
+		hashh = hashlib.sha1()
+		hashh.update(input_string.encode())
+		hashh = list(hashh.hexdigest())
 	key = [hashh[0], hashh[1], hashh[6], hashh[7], hashh[14], hashh[15], hashh[22], hashh[23], hashh[30], hashh[31]]
 
 	password = [''] * 10
